82d6258094f2abc473750ea27d7dbfdf06797e23
mateusz <matpdomo@gmail.com>
2024-03-10 18:46:28 -0700

    Successfully sending echo request and receiving echo reply

--- main.c
+++ main.c
@@ -16,31 +16,18 @@ struct icmp {
     uint16_t sequenceNumber;
 };
 
-u_int16_t checksum(struct icmp *packet);
+u_int16_t checksum(struct icmp* packet);
+int ping(int sockFD, char* address);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
     if (argc != 3) {
         printf("Incorrect Arguments\n");
         return 1;
     }
 
-    char *cmd =  argv[1];
-    char *destAddress =  argv[2];
-
-    char hostnameBuffer[256];
-    if (gethostname(hostnameBuffer, sizeof(hostnameBuffer)) == -1) {
-        printf("%s\n", strerror(errno));
-        return 1;
-    }
-    printf("%s\n", hostnameBuffer); 
-
-    struct hostent *host_entry;
-    host_entry = gethostbyname(hostnameBuffer);
-    char *IPbuffer;
-    IPbuffer = inet_ntoa(*((struct in_addr*)
-                        host_entry->h_addr_list[0]));
-    printf("%s\n", IPbuffer);
+    char* cmd =  argv[1];
+    char* destAddress =  argv[2];
 
     int sockFD = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sockFD == -1) {
@@ -49,32 +36,52 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    if (ping(sockFD, destAddress) == -1) {
+        printf("%s\n", strerror(errno));
+        close(sockFD);
+        return -1;
+    }
+
+    close(sockFD);
+    return 0;
+}
+
+int ping(int sockFD, char* address) {
     struct icmp packet;
     memset(&packet, 0, sizeof(packet));
     packet.type = 0x08;
     packet.code = 0x0;
-    packet.identifier = 0x1234;
-    packet.sequenceNumber = 0x1;
+    packet.identifier = htons(0x11);
+    packet.sequenceNumber = htons(0x01);
     packet.checksum = checksum(&packet);
 
     struct sockaddr_in  destinationAddress;
+    socklen_t destAddrLen = sizeof(destinationAddress);
     memset(&destinationAddress, 0, sizeof(destinationAddress));
     destinationAddress.sin_family = AF_INET;
     destinationAddress.sin_port = 0;
-    if (inet_pton(AF_INET, destAddress, &destinationAddress.sin_addr) == -1) {
-        printf("%s\n", strerror(errno));
-        close(sockFD);
+    if (inet_pton(AF_INET, address, &destinationAddress.sin_addr) == -1) {
+        return -1;
     }
 
-    int bytesSent = sendto(sockFD, &packet, sizeof(packet), 0, (const struct sockaddr *)&destinationAddress, sizeof(destinationAddress));
+    int bytesSent = sendto(sockFD, &packet, sizeof(packet), 0, (const struct sockaddr *)&destinationAddress, destAddrLen);
     if (bytesSent == -1) {
-        printf("%s\n", strerror(errno));
-        close(sockFD);
         return 1;
     }
-
     printf("Sent %d bytes\n", bytesSent);
-    close(sockFD);
+
+    char buffer[1024];
+    memset(&buffer, 0, sizeof(buffer));
+    printf("%ld\n", sizeof(buffer));
+    int bytesReceived = recvfrom(sockFD, &buffer, sizeof(buffer), 0, (struct sockaddr *)&destinationAddress, &destAddrLen);
+    if (bytesReceived == -1) {
+        return -1;
+    }
+    printf("Received %d bytes\n", bytesReceived);
+    for (int i = 0; i < bytesReceived; i++) {
+        printf("%02X ", (unsigned char)buffer[i]);
+    }
+    printf("\n");
     return 0;
 }
 
